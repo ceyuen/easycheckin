39a0f75ec8fab687f033765a3dd0bc95563c3ed4
Christina <christina.elaine.yuen@gmail.com>
2018-08-29 16:46:25 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Container } from 'semantic-ui-react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Container>
+          hello
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default App;
