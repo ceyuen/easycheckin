92f664413d9a78695e6172fff75b1c6170d68b44
Christina <christina.elaine.yuen@gmail.com>
2018-09-10 14:36:06 -0700

    added host for checkinrequest

--- src/checkInRequest.jsx
+++ src/checkInRequest.jsx
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
 export default class CheckInRequest {
+  hostName = 'http://192.168.1.100:57630/'
+
   addNewRequest = () => {
-    axios.put('api/checkinrequests')
+    axios.put(`${this.hostName}api/checkinrequests`)
       .then(() => {
         this.setState({addFlightError: true})
       })
